e3c0c856b8835f5e4193a5e78b83acbfac5eca64
Rajeshwari <eswarip111@gmail.com>
2026-04-16 22:34:18 +0530

    Fix PWA install

--- foodapp/static/serviceworker.js
+++ foodapp/static/serviceworker.js
@@ -1,3 +1,8 @@
 self.addEventListener('install', function(event) {
     console.log("Service Worker Installed");
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event) {
+    console.log("Service Worker Activated");
 });
